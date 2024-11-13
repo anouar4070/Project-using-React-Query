7f998cb57d6212e3df62caecb42bec8b49717cdb
anouar garbaa <g.anouar@yahoo.com>
2024-11-13 14:08:48 +0100

    used of combination: React Query & React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+//& *** in this version i used a combination: React Query & React Router ***
+
+
 import {
   Navigate,
   RouterProvider,
   createBrowserRouter,
 } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import Events from "./components/Events/Events.jsx";
 import EventDetails from "./components/Events/EventDetails.jsx";
 import NewEvent from "./components/Events/NewEvent.jsx";
-import EditEvent from "./components/Events/EditEvent.jsx";
-import { QueryClientProvider } from "@tanstack/react-query";
+import EditEvent, {
+  loader as editEventLoader,
+  action as editEventAction,
+} from "./components/Events/EditEvent.jsx";
 import { queryClient } from "./util/http.js";
 
 const router = createBrowserRouter([
@@ -34,11 +40,12 @@ const router = createBrowserRouter([
       {
         path: "/events/:id/edit",
         element: <EditEvent />,
+        loader: editEventLoader,
+        action: editEventAction,
       },
     ],
   },
 ]);
-
 
 function App() {
   return (
@@ -50,51 +57,3 @@ function App() {
 
 export default App;
 
-
-
-
-
-// import {
-//   Navigate,
-//   RouterProvider,
-//   createBrowserRouter,
-// } from 'react-router-dom';
-
-// import Events from './components/Events/Events.jsx';
-// import EventDetails from './components/Events/EventDetails.jsx';
-// import NewEvent from './components/Events/NewEvent.jsx';
-// import EditEvent from './components/Events/EditEvent.jsx';
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Navigate to="/events" />,
-//   },
-//   {
-//     path: '/events',
-//     element: <Events />,
-
-//     children: [
-//       {
-//         path: '/events/new',
-//         element: <NewEvent />,
-//       },
-//     ],
-//   },
-//   {
-//     path: '/events/:id',
-//     element: <EventDetails />,
-//     children: [
-//       {
-//         path: '/events/:id/edit',
-//         element: <EditEvent />,
-//       },
-//     ],
-//   },
-// ]);
-
-// function App() {
-//   return <RouterProvider router={router} />;
-// }
-
-// export default App;
---
+++ src/components/HeaderOld.jsx
@@ -0,0 +1,16 @@
+// *** in this version i use the optimistic update - React Query only ***
+
+
+export default function Header({ children }) {
+  return (
+    <>
+      <div id="main-header-loading"></div>
+      <header id="main-header">
+        <div id="header-title">
+          <h1>React Events</h1>
+        </div>
+        <nav>{children}</nav>
+      </header>
+    </>
+  );
+}
